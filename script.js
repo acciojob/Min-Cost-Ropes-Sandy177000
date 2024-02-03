b94ff380fd6a38667aa71426933e9dc9779adc36
Sandesh <112424645+Sandy177000@users.noreply.github.com>
2024-02-03 16:39:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function mincost(arr)
 { 
-//write your code here
-// return the min cost
-  
+	let pq = new PriorityQueue();
+
+	for (let index = 0; index < arr.length; index++) {
+		pq.enqueue(arr[index]);
+	}
+
+	let x = 0 ;
+	while(pq.size()>1){
+		let f = pq.dequeue();
+		let s = pq.dequeue();
+		x+= f+s;
+		pq.enqueue(f+s);
+	}
+
+	return x;
 }
 
 module.exports=mincost;
